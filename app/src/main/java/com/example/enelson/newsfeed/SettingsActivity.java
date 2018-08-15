adfd926c466ee8199c7eb1517ead71e0c6e025aa
Elizabeth Nelson <ENelson@travisusd.org>
2018-08-15 11:01:55 -0700

    preference with a list preference with two radio dial buttons works correctly

--- app/src/main/java/com/example/enelson/newsfeed/SettingsActivity.java
+++ app/src/main/java/com/example/enelson/newsfeed/SettingsActivity.java
@@ -16,8 +16,13 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
     }
 
-    public static class NewsPreferenceFragment extends PreferenceFragment
-    {
+    public static class NewsPreferenceFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
 
     }
 }
